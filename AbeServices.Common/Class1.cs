5591f9b940df734b32b9acb2303db9960a97f583
Kirill Yurevich Ponomarev <drmckay-kirill@yandex.ru>
2019-09-15 19:48:53 +0500

    init

---
+++ AbeServices.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AbeServices.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ AbeServices.DeviceEmulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbeServices.DeviceEmulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
